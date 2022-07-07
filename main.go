0951d5d178f5326b32b020217f71712f5f4b60ef
jeadie <jackeadie@duck.com>
2022-07-07 10:38:48 +1000

    structure of program

--- main.go
+++ main.go
@@ -1,24 +1,21 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"os"
 )
-import "github.com/jomei/notionapi"
 
 func main() {
-	key, exists := os.LookupEnv("NOTION_KEY")
-	if !exists {
-		fmt.Printf("`NOTION_KEY` not set.")
-		return
-	}
 
-	client := notionapi.NewClient(notionapi.Token(key))
-	u, err := client.User.Get(context.Background(), "me")
+	nDao, err := ConstructNotionDaoFromEnv()
 	if err != nil {
 		fmt.Printf(err.Error())
 		return
 	}
-	fmt.Printf("Hello %s!\n", u.Name)
+
+	rssUrls := nDao.GetEnabledRssFeeds()
+	rssContent := GetRssContent(rssUrls, 24)
+	for item := range rssContent {
+		nDao.AddRssItem(item)
+	}
+
 }
