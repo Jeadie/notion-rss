338f425123ace12944fb2e083d4141cc03abf07b
jeadie <jackeadie@duck.com>
2022-07-07 11:00:03 +1000

    add basic RSS support

--- go.mod
+++ go.mod
@@ -3,6 +3,18 @@ module github.com/Jeadie/notion-rss
 go 1.18
 
 require (
-	github.com/jomei/notionapi v1.8.5 // indirect
+	github.com/jomei/notionapi v1.8.5
+	github.com/mmcdole/gofeed v1.1.3
+)
+
+require (
+	github.com/PuerkitoBio/goquery v1.5.1 // indirect
+	github.com/andybalholm/cascadia v1.1.0 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/mmcdole/goxpp v0.0.0-20181012175147-0068e33feabf // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/net v0.0.0-20200301022130-244492dfa37a // indirect
+	golang.org/x/text v0.3.2 // indirect
 )
--- rss.go
+++ rss.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"github.com/mmcdole/gofeed"
+	"github.com/mmcdole/gofeed/rss"
 	"net/url"
+	"strings"
 	"time"
 )
 
@@ -11,22 +15,53 @@ type RssItem struct {
 	content []string
 }
 
+// GetRssContent from a channel of RSS urls, parses new RSS items (that are from the lastNHours),
+// and sends them to an output channel.
 func GetRssContent(urls chan *url.URL, lastNHours int) chan RssItem {
 	result := make(chan RssItem)
 
 	go func(urls chan *url.URL, lastNHours int) {
 		defer close(urls)
-		timeSince := time.Now().Add(-1 * time.Hour * lastNHours)
+		timeSince := time.Now().Add(-1 * time.Hour * time.Duration(lastNHours))
 
 		rssContent := make(chan *RssItem)
 		for u := range urls {
-			rssContent <- GetRssContentFrom(u, timeSince)
+			for _, item := range GetRssContentFrom(u, timeSince) {
+				rssContent <- item
+			}
 		}
 	}(urls, lastNHours)
 
 	return result
 }
 
-func GetRssContentFrom(url *url.URL, since time.Time) *RssItem {
+// GetRssContentFrom since afterTime from the RSS feed found at url.
+func GetRssContentFrom(url *url.URL, afterTime time.Time) []*RssItem {
+	fp := gofeed.NewParser()
+	feed, err := fp.ParseURL(url.String())
+	if err != nil {
+		fmt.Println(fmt.Errorf("could not get content from rss url: %s. Error occurred %w", url, err).Error())
+		return []*RssItem{}
+	}
 
+	result := make([]*RssItem, len(feed.Items))
+
+	count := 0
+	for _, item := range feed.Items {
+		if item.PublishedParsed.After(afterTime) {
+			result[count] = convert(item)
+			count++
+		}
+	}
+	return result[:count]
+}
+
+// convert gofeed.Item into an internal RSSItem model.
+func convert(item *gofeed.Item) *RssItem {
+	link, _ := url.Parse(item.Link)
+	return &RssItem{
+		title:   item.Title,
+		link:    *link,
+		content: []string{item.Content},
+	}
 }
