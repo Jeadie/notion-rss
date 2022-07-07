17a3311143072b35199df64cb9ed98b8debfd6f5
jeadie <jackeadie@duck.com>
2022-07-08 08:04:10 +1000

    improve error handling in main.go

--- main.go
+++ main.go
@@ -9,16 +9,24 @@ func main() {
 
 	nDao, err := ConstructNotionDaoFromEnv()
 	if err != nil {
-		fmt.Printf(err.Error())
-		return
+		panic(fmt.Errorf("configuration error: %w", err))
 	}
 
 	rssUrls := nDao.GetEnabledRssFeeds()
+	last24Hours := time.Now().Add(-1 * time.Hour * time.Duration(24))
+	rssContent := GetRssContent(rssUrls, last24Hours)
 
-	timeSince := time.Now().Add(-1 * time.Hour * time.Duration(24))
-	rssContent := GetRssContent(rssUrls, timeSince)
+	failedCount := 0
 	for item := range rssContent {
-		nDao.AddRssItem(item)
+		err := nDao.AddRssItem(item)
+		if err != nil {
+			fmt.Printf("Could not create page for %s, URL: %s. Error: %s\n", item.title, item.link, err.Error())
+			failedCount++
+		}
 	}
 
+	// Fail after all RSS items are processed to minimise impact.
+	if failedCount > 0 {
+		panic(fmt.Errorf("%d Rss item/s failed to be created in the notion database. See errors above", failedCount))
+	}
 }
