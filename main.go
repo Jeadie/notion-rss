beebd3332439458e906695f98cd538e8a18f689c
jeadie <jackeadie@duck.com>
2022-07-11 15:09:05 +1000

    archive old unstarred pages after 30 days and gracefully handle multiple potential errors

--- main.go
+++ main.go
@@ -12,6 +12,46 @@ func main() {
 		panic(fmt.Errorf("configuration error: %w", err))
 	}
 
+	newContentErr := AddNewContent(nDao)
+	archiveErr := ArchiveOldUnstarredContent(nDao)
+
+	PanicOnErrors([]error{newContentErr, archiveErr})
+}
+
+// PanicOnErrors prints all non-nil err in errors and panics if there is at least one non-nil
+// error in errors. Otherwise, return normally.
+func PanicOnErrors(errors []error) {
+	// Only used if one error (for better error handling).
+	var firstErr error
+	errN := 0
+
+	// Print all non-nil errors.
+	for _, err := range errors {
+		if err != nil {
+			fmt.Printf("%s\n", err.Error())
+			errN++
+			firstErr = err
+		}
+	}
+
+	// Multiple errors, panic with generic message.
+	if errN > 1 {
+		panic(fmt.Errorf("Multiple errors occured. Check output for details"))
+	}
+
+	if errN == 1 {
+		panic(firstErr)
+	}
+}
+
+// ArchiveOldUnstarredContent from the content database that is older than 30 days and is not starred.
+func ArchiveOldUnstarredContent(nDao *NotionDao) error {
+	pageIds := nDao.GetOldUnstarredRSSItems(time.Now().Add(-30 * time.Hour * time.Duration(24)))
+	return nDao.ArchivePages(pageIds)
+}
+
+// AddNewContent from all enabled RSS Feeds that have been published within the last 24 hours.
+func AddNewContent(nDao *NotionDao) error {
 	rssFeeds := nDao.GetEnabledRssFeeds()
 	last24Hours := time.Now().Add(-1 * time.Hour * time.Duration(24))
 	rssContent := GetRssContent(rssFeeds, last24Hours)
@@ -27,6 +67,7 @@ func main() {
 
 	// Fail after all RSS items are processed to minimise impact.
 	if failedCount > 0 {
-		panic(fmt.Errorf("%d Rss item/s failed to be created in the notion database. See errors above", failedCount))
+		return fmt.Errorf("%d Rss item/s failed to be created in the notion database. See errors above", failedCount)
 	}
+	return nil
 }
